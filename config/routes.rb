0416efc8dd89fbc140851b089ef97b8c35e5a37e
asnr <asnr@users.noreply.github.com>
2017-04-01 14:40:02 +1100

    Add static signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
+  get '/signup',  to: 'users#new'
   get '/contact', to: 'static_pages#contact'
 end
