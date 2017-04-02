e22d471ca32593cadd83209ef8502a67fc2db2a7
asnr <asnr@users.noreply.github.com>
2017-04-03 09:50:04 +1000

    Finish signup flow

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test 'invalid signup information' do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: '', email: 'user@invalid',
+                                          password: 'foo',
+                                          confirmation: 'bar'} }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation div.alert', /.+3 errors.+/
+    assert_select 'div#error_explanation ul > li', 3
+  end
+
+  test 'valid signup information' do
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { name: 'A person',
+                                          email: 'a@person.com',
+                                          password: 'password',
+                                          confirmation: 'password' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
