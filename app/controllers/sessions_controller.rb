021d344d816e474eaf6d2dd97442f9f7b07c1952
asnr <asnr@users.noreply.github.com>
2017-04-21 17:53:30 +0800

    Add user activation via emails

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,16 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      if @user.activated
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        message = 'You must activate your account before you can log in.'
+        message += ' Check yo email.'
+        flash[:warning] = message
+        redirect_to root_path
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
---
+++ test/controllers/account_activations_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AccountActivationsControllerTest < ActionDispatch::IntegrationTest
+  test "account is not activated with incorrect details" do
+    assert true
+  end
+end
